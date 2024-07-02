69044d74fbb31723f5ec8424e39052e5792baab9
Pippo <90771656+Pimmler1@users.noreply.github.com>
2024-07-02 17:32:36 +0200

    Hinzufügen von Dokumenten

--- Code/Smartphoneapp/HttpTest/App.js
+++ Code/Smartphoneapp/HttpTest/App.js
@@ -3,20 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://de1.api.radio-browser.info/json/stations/search", true);
-  xhttp.onreadystatechange = () => {
-    if(this.readyState == 4 && this.status == 200){
-      console.log(this.responseText);
-    }
-  }
-  const data = {
-    "country": "Germany"
-  }
-  xhttp.send(data);
+  const resp = getStations();
+  console.log(resp);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Yolo</Text>
       <StatusBar style="auto" />
     </View>
   );
